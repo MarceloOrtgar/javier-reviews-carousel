b2ef270e9859d93ad35ee6e5150791671e444dbe
MarceloOrtgar <marceloortega18@gmail.com>
2022-05-12 18:58:27 -0400

    first commit

--- index.js
+++ index.js
@@ -1,80 +1,37 @@
-// Every person's review
-const reviews = [
-    {
-        name: 'Haley Burrell',
-        image: 'retrato-1.jpg',
-        job: 'Lawyer',
-        review: 'Best Portal for Files, Billing and Communication with clients.'
-    },
-    {
-        name: 'Vincent Wilson',
-        image: 'retrato-2.jpg',
-        job: 'Entrepreneur',
-        review: 'Excellent solution for files-sharing and eSignatures.'
-    },
-    {
-        name: 'Clarence Donaldson',
-        image: 'retrato-3.jpg',
-        job: 'Engineer',
-        review: 'By far the best designed client portal product.'
-    },
-    {
-        name: 'Chloe Griffin',
-        image: 'retrato-4.jpg',
-        job: 'Graphic Designer',
-        review: 'Messaging, file-sharing, and billing in one place!'
-    },
-    {
-        name: 'Rose Miller',
-        image: 'retrato-5.jpg',
-        job: 'Front-end Developer',
-        review: 'Amazing white label file sharing & eSignature solution.'
-    }
-];
+const boton = document.querySelector("#random-btn");
+const name = document.querySelector("#name");
+const job = document.querySelector("#job");
+const image = document.querySelector("#image");
+const opinion = document.querySelector("#opinion");
 
-// Variables to select the DOM elements
-const person = document.getElementById('name');
-const image = document.getElementById('image');
-const job = document.getElementById('job');
-const review = document.querySelector('#opinion p');
-const btns = document.querySelectorAll('.btn');
+console.log(name);
+console.log(image);
+console.log(job);
+console.log(opinion);
 
-// Variable of Index
-let index = 0;
-
-// Shows the review
-function showReview() {
-    const item = reviews[index];
-    person.textContent = item.name;
-    image.src = item.image;
-    job.textContent = item.job;
-    review.textContent = item.review;
-}
-
-// Load initial review
-window.addEventListener('DOMContentLoaded', function () {
-    showReview();
-});
-
-// Function of all the buttons
-btns.forEach(btn => {
-    btn.addEventListener('click', function () {
-        const btnClass = btn.classList;
-
-        if (btnClass.contains('prev')) {
-            index--;
-        } else if (btnClass.contains('next')) {
-            index++;
-        } else {
-            index = Math.floor(Math.random() * reviews.length);
-        }
-
-        if (index < 0) {
-            index = reviews.length - 1;
-        } else if (index >= reviews.length) {
-            index = 0;
-        }
-
-        showReview();
-    })
+boton.addEventListener("click", (e) => {
+  
+    console.log(name.textContent)
+  
+    if (name.textContent === "name retraro 4") {
+    name.textContent = "name retraro 1";
+    image.src = "retrato-1.jpg";
+    job.textContent = "job retraro 1";
+    opinion.textContent = "opinion retrato 1";
+  } else if (name.textContent === "name retraro 1") {
+    name.textContent = "name retraro 2";
+    image.src = "retrato-2.jpg";
+    job.textContent = "job retraro 2";
+    opinion.textContent = "opinion retrato 2";
+  } else if (name.textContent === "name retraro 2") {
+    name.textContent = "name retraro 3";
+    image.src = "retrato-3.jpg";
+    job.textContent = "job retraro 3";
+    opinion.textContent = "opinion retrato 3";
+  } else {
+    name.textContent = "name retraro 4";
+    image.src = "retrato-4.jpg";
+    job.textContent = "job retraro 4";
+    opinion.textContent = "opinion retrato 4";
+  }
 });
